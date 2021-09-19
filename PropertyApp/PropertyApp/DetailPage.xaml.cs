14531e4fbf5274599eacb29ad0b4adbab33c17e5
yunushamod <yunushamod@gmail.com>
2021-09-19 15:42:59 +0100

    Add project files.

---
+++ PropertyApp/PropertyApp/DetailPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PropertyApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DetailPage : ContentPage
+    {
+        public DetailPage(Property property)
+        {
+            InitializeComponent();
+            this.Property = property;
+            this.BindingContext = this;
+        }
+        public Property Property { get; private set; }
+        public void GoBack(object sender, EventArgs e)
+        {
+            this.Navigation.PopAsync();
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            DetailView.TranslationY = 600;
+            DetailView.TranslateTo(0, 0, 500, Easing.SinInOut);
+        }
+    }
+}
